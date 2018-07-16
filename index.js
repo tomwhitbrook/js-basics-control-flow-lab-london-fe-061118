877193bdf3e01c9a9a31e41c361d50941dce375f
Tom Whitbrook <tomwhitbrook@gmail.com>
2018-07-16 17:11:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance < 400) {
+    return "This one is on me!"
+  }
+  else if (distance > 2500) {
+    return "No can do."
+  }
+  else if (distance > 2000) {
+    return "I will gladly take your thirty bucks."
+  }
+}
+
+function ternaryCheckCity(city) {
+  return (city === 'NYC') ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case "generous":
+        return "Thank you so much."
+        break;
+     case "not as generous":
+        return "Thank you."
+        break;
+    default:
+        return "Bye."
+}
+}
